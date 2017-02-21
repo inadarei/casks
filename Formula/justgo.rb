cfc4462aeeb1a31870ebe9a2d9fd52dedcc26a12
Irakli N <inadarei@users.noreply.github.com>
2017-02-21 01:57:08 -0500

    init

---
+++ Formula/justgo.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class JustGo < Formula
+  desc "Quickly create a skeleton project for Go microservices"
+  homepage "https://github.com/inadarei/justgo"
+  version "1.0.1"
+  if OS.mac?
+    url "https://github.com/inadarei/justgo/archive/v1.0.1.tar.gz"
+    sha256 "4107e9ee5acf246fa633e7314adddb5f9e97ae24857b0a7429625864bc4d851c"
+  end
+
+  def install
+      bin.install cmd/justgo/build/justgo
+      rm_rf Dir['**/*']
+  end
+
+end
