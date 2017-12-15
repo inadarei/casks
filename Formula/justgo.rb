9bfee3f1b87b623b4cef0976bfb9a2f3d68b2e0e
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2017-12-15 18:18:55 -0500

    Update justgo.rb

--- Formula/justgo.rb
+++ Formula/justgo.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Justgo < Formula
   desc "Quickly create a skeleton project for Go microservices"
   homepage "https://github.com/inadarei/justgo"
-  version "1.1.2"
+  version "1.1.3"
   if OS.mac?
     url "https://github.com/inadarei/justgo/archive/v1.1.2.tar.gz"
     sha256 "a10a5e54658bbbb3e56ad701ff0ef0cf15ed18602f63af15ce32492d8ade5e7c"
