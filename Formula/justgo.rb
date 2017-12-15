d1177a2e4bb413510f41526b58d7bcf501a3abcc
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2017-12-15 18:21:28 -0500

    Update justgo.rb

--- Formula/justgo.rb
+++ Formula/justgo.rb
@@ -5,7 +5,7 @@ class Justgo < Formula
   homepage "https://github.com/inadarei/justgo"
   version "1.1.3"
   if OS.mac?
-    url "https://github.com/inadarei/justgo/archive/v1.1.2.tar.gz"
+    url "https://github.com/inadarei/justgo/archive/v1.1.3.tar.gz"
     sha256 "a10a5e54658bbbb3e56ad701ff0ef0cf15ed18602f63af15ce32492d8ade5e7c"
   end
 
